0bf68b0808dd214374bb3f8b29f8079d48a80c46
Michael Novotny <manovotny@gmail.com>
2016-02-20 14:32:22 -0600

    Adds dist creation.

--- grunt/grunt-contrib-clean.js
+++ grunt/grunt-contrib-clean.js
@@ -9,27 +9,8 @@ module.exports = function (grunt) {
             config.paths.source + '/**/css',
             config.paths.source + '/**/' + config.files.browserify + '.*.js'
         ],
-        deploy: [
-            '**/.DS_Store',
-            '**/.git*',
-            '**/bower.json',
-            '**/composer.json',
-            '**/composer.lock',
-            '**/LICENSE',
-            '**/package.json',
-            '**/README.md',
-
-            '**/*.ai',
-            '**/*.psd',
-            '**/*.pxm',
-            '**/*.sketch',
-
-            '**/' + config.paths.curl + '/**',
-
-            '**/bower_components/**',
-            '**/less/**',
-            '**/sass/**',
-            '**/tests/**'
+        release: [
+            'dist'
         ]
     });
 
--- grunt/grunt-shell.js
+++ grunt/grunt-shell.js
@@ -12,6 +12,12 @@ module.exports = function (grunt) {
         refresh: {
             command: 'composer dump-autoload'
         },
+        release: {
+            command: 'composer update --prefer-dist --no-interaction --no-dev --optimize-autoloader -vvv'
+        },
+        reset: {
+            command: 'composer update'
+        },
         update: {
             command: [
                 'npm prune',
